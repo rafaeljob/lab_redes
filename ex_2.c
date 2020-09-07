3e4c337da8137a43b1da0602dac8d0706ff337d8
rafael <rafael.basso@acad.pucrs.br>
2020-09-07 15:43:21 -0300

    add ex2

--- ex_2.c
+++ ex_2.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <curses.h>
  
 typedef void *(*state_func)();
 
 int timer = 0, lock_act = 0, unlock_act = 0, timer_start = 0, locked = 0, unlocked = 0;
-int c, button = 0;
-int vel = 0;
-
+int c, button = 0, vel = 0, lock = 1;
+int door[4] = {1,1,1,1};
 /* 
 botao 1 = trava
 botao 2 = destrava
@@ -15,53 +15,104 @@ botao 2 = destrava
 caso velocidade acima de 30 trava portas
 */
 
+int kbhit(void);
+void *idle();
+void *un_lock();
+void *unlock_all();
+void *lock_back();
+void *lock_all();
 
-void *idle_unlocked();
-void *idle_locked();
-void *lock();
-void *unlock();
-
-void *idle_unlocked() {
-	timer = 0; lock_act = 0; locked = 0; unlock_act = 0; unlocked = 1; timer_start = 0;
-	if((vel > 30 || button == 1)) { return locked;}
-	if(button == 0) { return idle_unlocked;}
-	return 0;
-}
-
-void *idle_locked() {
-	timer = 0; lock_act = 0; locked = 1; unlock_act = 0; unlocked = 0; timer_start = 0;
-	if(button == 2) { return unlocked;}
-	if(button == 0) { return idle_locked;}
-	return 0;
-}
-
-void *lock() {
+void *un_lock() {
 	timer++;
-	lock_act = 1; locked = 0; unlock_act = 0; unlocked = 1; timer_start = 1;
-	if(timer < 5) { return lock;}
-	if(!(timer < 5)) { return idle_locked;}
+	timer_start = 1;
+	if(timer < 5) { return un_lock;}
+	if(!(timer < 5)) {
+		if(door[button - 1] == 1) {
+			printf("\nUNLOCKING %d", button);
+			door[button - 1] = 0;
+		} else {
+			printf("\nLOCKING %d", button);
+			door[button - 1] = 1;
+		}
+		button = 0;
+		return idle;
+	}
 	return 0;
 }
 
-void *unlock() {
+void *idle() {
+	timer = 0; timer_start = 0;
+	if(vel > 30) { return lock_all;}
+	if(button == 5) { 
+		if(lock == 1) { return unlock_all;}
+		else { return lock_all;}
+	}
+	if(button == 6) { return lock_back;}
+	if(button == 0) { return idle;}
+	if(button < 5) { return un_lock;}
+	return 0;
+}
+
+void *unlock_all() {
 	timer++;
-	lock_act = 0; locked = 1; unlock_act = 1; unlocked = 0; timer_start = 1;
-	if(timer < 5) { return unlock;}
-	if(!(timer < 5)) { return idle_unlocked;}
+	unlock_act = 1; timer_start = 1;
+	if(timer < 5) { return unlock_all;}
+	if(!(timer < 5)) {
+		printf("\nUNLOCKING ALL");
+		for(int i = 0;i < 4;i++) {
+			door[i] = 0;
+		}
+		lock = 0;
+		button = 0;
+		return idle;
+	}
+	return 0;
+}
+
+void *lock_all() {
+	timer++;
+	timer_start = 1;
+	if(timer < 5) { return lock_all;}
+	if(!(timer < 5)) {
+		printf("\nLOCKING ALL");
+		for(int i = 0;i < 4;i++) {
+			door[i] = 1;
+		}
+		lock = 1;
+		button = 0;
+		return idle;
+	}
+	return 0;
+}
+
+void *lock_back() {
+	timer++;
+	timer_start = 1;
+	if(timer < 5) { return lock_back;}
+	if(!(timer < 5)) {
+		printf("\nLOCKING BACK");
+		door[2] = 1;
+		door[3] = 1;
+		button = 0;
+		return idle;
+	}
 	return 0;
 }
 
 int main() {
-	state_func curr_state = idle_locked;
+	state_func curr_state = idle;
 
 	while(1) {
 		curr_state = (state_func)(*curr_state)();
-		printf("locked: %d, unlocked: %d, locking: %d, unlocking: %d, speed: %d \n", locked, unlocked, lock_act, unlock_act, vel);
-		if (kbhit()) {
-			c = getchar();
-			printf("%c", c);
-		}
+		printf("\ndoor locked 1: %d\ndoor locked 2: %d\ndoor locked 3: %d\ndoor locked 4: %d\nspeed: %d , button: %d\n", door[0], door[1], door[2], door[3], vel, button);
 		sleep(1);
+		if(kbhit()) {
+			c = getchar();
+			printf("\ninput: %c", c);
+			if(c == 'a') {vel = vel + 10;}
+			if(c == 'd') {if(vel > 0) vel = vel - 10;}
+			if(c <= '6') {button = c - 48;}
+		}
 	}
 	return 0;
 }
